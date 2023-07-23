b7ed230766f2f8713a33415e850088f4ff5fceae
Dylan Chen <zilin@singularity-data.com>
2023-07-23 16:03:46 +0800

    forbid incremental

---
+++ dbt/include/risingwave/macros/materializations/incremental.sql
@@ -0,0 +1,10 @@
+{% materialization incremental, adapter='risingwave' %}
+    {{ exceptions.CompilationError(
+        """
+        dbt-risingwave does not support incremental models, but we provide a `materializedview` model 
+        which could keep your data up to date automatically and incrementally.
+
+        Use the `materializedview` instead.
+        """
+    )}}
+{% endmaterialization %}
